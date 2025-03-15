a4fcf92a9cf1f55f92db75a6b8acbac0196ee192
Grace Xu <xuweijia222@gmail.com>
2025-03-14 21:46:25 -0700

    add first component

---
+++ my-first-component-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return <MyFirstComponent />;
+}
+
+function MyFirstComponent() {
+  return (
+    <div>
+      <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+    </div>
+  );
+}
+
+export default App;
